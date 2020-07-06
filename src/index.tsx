0e1ca1092ff54a87334c4e83d89460afd034f26d
grgr-dkrk <shayashi513@gmail.com>
2020-07-06 13:40:04 +0900

    fix: initial render

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import * as ReactDOM from 'react-dom'
 import { App } from './app'
 import { BrowserRouter } from 'react-router-dom'
 
-const appRoot = document.getElementById('app')!
+const appRoot = document.getElementById('app')
 const isConcurrent = process.env.RENDER_MODE === 'concurrent'
-const isRender =
-  document.getElementById('root')?.childElementCount === 0 ?? false
+const isRender = appRoot?.childElementCount === 0 ?? false
 const renderWrapper = isRender ? ReactDOM.render : ReactDOM.hydrate
 
 console.log(`mode: ${isConcurrent ? 'concurrent' : 'legacy'}`)
